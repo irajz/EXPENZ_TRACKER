c5310d0b718560d728d80efd90cea591c49788a9
iraj oshadha <oshadairaj33@gmail.com>
2025-08-11 23:36:57 +0530

    installed the json web token dependency.

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ const transactionRoutes = require("./routes/transactionRoutes");
 app.use("/api", userRoutes);
 app.use("/api", transactionRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
